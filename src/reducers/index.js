9fa073bd7f9168df226142b504116a5b97f1e006
Uche Ozoemena <ucheozoemena@gmail.com>
2018-05-03 05:46:12 +0100

    Map PostShownReducer to postShown piece of state

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
 import PostsReducer from './reducer_posts';
+import PostShownReducer from './reducer_post_shown';
 
 const rootReducer = combineReducers({
   posts: PostsReducer,
+  postShown: PostShownReducer,
   form: formReducer
 });
 
