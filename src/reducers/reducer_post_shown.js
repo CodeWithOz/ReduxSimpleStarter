d5495359428241bc120ee2ba488484d9715b4d7b
Uche Ozoemena <ucheozoemena@gmail.com>
2018-05-03 08:55:30 +0100

    Add DELETE_POST case to PostShownReducer

--- src/reducers/reducer_post_shown.js
+++ src/reducers/reducer_post_shown.js
@@ -1,10 +1,13 @@
 import _ from 'lodash';
-import { fetchPost, FETCH_POST } from '../actions';
+import { fetchPost, FETCH_POST, DELETE_POST } from '../actions';
 
 export default function(state = {}, action) {
   switch (action.type) {
     case FETCH_POST:
       return { [action.payload.data.id]: action.payload.data };
+      break;
+    case DELETE_POST:
+      return {};
       break;
     default:
       return state;
